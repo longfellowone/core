1fc3ee6642f85782a9e2e10ba6e64b0311cf72c0
Matt Wright <mattwright3195@gmail.com>
2018-12-10 20:50:04 -0800

    working nav

--- src/Order/Dashboard.jsx
+++ src/Order/Dashboard.jsx
@@ -2,19 +2,18 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 export const Dashboard = () => {
-  console.log();
   return (
     <div>
       <div>Dashboard</div>
       <ul className="list-reset">
         <li>
-          <Link to="/new">New Order</Link>
+          <Link to="/order/new">New Order</Link>
         </li>
         <li>
-          <Link to="/receive/">Recieve Order</Link>
+          <Link to="/order/receive/">Recieve Order</Link>
         </li>
         <li>
-          <Link to="/history/">Order History</Link>
+          <Link to="/order/history/">Order History</Link>
         </li>
       </ul>
     </div>
--- src/Order/Order.jsx
+++ src/Order/Order.jsx
@@ -1,7 +1,17 @@
 import React from 'react';
-//import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import { Dashboard } from './Dashboard';
+import { NewOrder } from './NewOrder';
+import { ReceiveOrder } from './ReceiveOrder';
+import { OrderHistory } from './OrderHistory';
 
-export const Order = () => {
-  return <Dashboard />;
+export const Order = ({ match }) => {
+  return (
+    <Switch>
+      <Route path={match.url + '/'} exact component={Dashboard} />
+      <Route path={match.url + '/new'} component={NewOrder} />
+      <Route path={match.url + '/receive'} component={ReceiveOrder} />
+      <Route path={match.url + '/history'} component={OrderHistory} />
+    </Switch>
+  );
 };
